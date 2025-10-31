2727d940cc8a4bf7418d9ca9c5096b521aa7d893
dnielclv4free <danielcalvin822@gmail.com>
2025-10-31 07:24:19 +0700

    update : create home page

--- niels_news/src/App.jsx
+++ niels_news/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
+import Home from "./pages/Home.jsx";
 import NewsList from './pages/NewsList.jsx';
 import NewsDetail from './pages/NewsDetail.jsx';
 import Footer from './components/Footer.jsx';
@@ -19,7 +20,7 @@ function App() {
       
       <main className="pt-20">
         <Routes>
-          <Route path="/" element={<NewsList />} />
+          <Route path="/" element={<Home />} />
           <Route path="/category/:category" element={<NewsList />} />
           <Route path="/search" element={<NewsList />} />
           <Route path="/article" element={<NewsDetail />} />
